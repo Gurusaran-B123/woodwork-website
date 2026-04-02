e030b12ab0ecf41cf82d85b155bcd94bd1c77bab
Gurusaran-B123 <mr.gurusaran123@gmail.com>
2026-04-01 20:29:58 -1100

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def get_db_connection():
         password=os.environ.get('MYSQLPASSWORD'),
         database=os.environ.get('MYSQLDATABASE'),
         port=int(os.environ.get('MYSQLPORT', 3306)),
+        ssl={'ssl': {}},
         cursorclass=pymysql.cursors.DictCursor
     )
 
